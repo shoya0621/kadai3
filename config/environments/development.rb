26bf6e8e27d95ccf02a0dddee14280eb708778c6
shoya0621 <122379046+shoya0621@users.noreply.github.com>
2023-02-23 15:07:13 +0000

    1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,8 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
+  
+  config.active_job.queue_adapter = :inline
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
@@ -73,4 +74,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "dca49d69413749789971848b6af09735.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
